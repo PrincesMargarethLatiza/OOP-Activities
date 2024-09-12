eefd3dc5c3d44851be9cfdc7b7b0473b4f9bd5c9
PrincesMargarethLatiza <margarethlatiza@gmail.com>
2024-09-12 15:06:49 +0800

    first commit

---
+++ Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    String name;
+    boolean isMale;
+    int numbers;
+    String dog[] = new String[5];
+    Dog puppies;
+
+public String bark(String name,int numbers){
+    return "hans" + name + "my name is " + name;
+}
+public String[] showPuppies(){
+ return dog;
+}
+public void addDog(String[]newDog){
+    dog= newDog;
+}
+public
+}
+
