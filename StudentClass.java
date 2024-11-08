61befbf4b4f59780e59e7b372dceae4df5146732
PrincesMargarethLatiza <margarethlatiza@gmail.com>
2024-11-08 13:30:50 +0800

    Add files via upload
    Activity 2.1

---
+++ StudentClass.java
@@ -0,0 +1,11 @@
+public class StudentClass{
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.setId(1234);
+        student1.setname("Student_1");
+
+        System.out.println("Student Id: " + student1.getId());
+        System.out.println("STudent Name: " + student1.getname());
+    }
+}
+ 
