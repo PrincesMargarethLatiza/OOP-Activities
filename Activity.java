8e0e602cf661022ea0cee021c73c7a8feccde9da
PrincesMargarethLatiza <margarethlatiza@gmail.com>
2024-11-22 13:16:39 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,26 +1,20 @@
-
 public class Activity {
     public static void main(String[] args) {
-Puppies puppies1= new Puppies ();
-Puppies puppies2= new Puppies ();
-Puppies puppies3= new Puppies ();
-Puppies puppies4= new Puppies ();
-Puppies puppies5= new Puppies ();
-
-puppies1.setName("Hans");
-puppies2.setName("Zhan");
-puppies3.setName("Yhan");
-puppies4.setName("Shan");
-puppies5.setName("Khan");
-
-puppies1.isMale=true;
-puppies2.isMale=true;
-puppies3.isMale=true;
-puppies4.isMale=true;
-puppies5.isMale=true;
-
-  
-  }
- }
+        
+        Student student = new Student("Princes", "Balayan", "BSIT", 2);
+        System.out.println("Student Name: " + student.getName());
+        System.out.println("Student Address: " + student.getAddress());
+        System.out.println("student Program: " + student.getProgram());
+        System.out.println("Student Year: " + student.getYear());
+        System.out.println(student.toString());
+        
+        Staff staff = new Staff("Margareth", "Lanatan", "BSU", 250000);
+        System.out.println("Staff Name: " + staff.getName());
+        System.out.println("Staff Address: " + staff.getAddress());
+        System.out.println("Staff School: " + staff.getSchool());
+        System.out.println("Staff Salary: " + staff.getSalary());
+        System.out.println(staff.toString());
+    }
+}
 
 
